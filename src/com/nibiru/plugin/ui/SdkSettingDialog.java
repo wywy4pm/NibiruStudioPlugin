2238761f39f2cbf6281c6a28dda1ad55134076ee
wuyong <arun.wu@inibiru.com>
2020-04-01 07:24:44 +0000

    sdk路径相同不执行导包

--- src/com/nibiru/plugin/ui/SdkSettingDialog.java
+++ src/com/nibiru/plugin/ui/SdkSettingDialog.java
@@ -119,21 +119,21 @@ public class SdkSettingDialog extends DialogWrapper {
                 String modulePath = ModuleUtils.getModulePath(project, folder);
                 if (!StringUtils.isBlank(modulePath)) {
                     String preSdkPath = PropertiesUtils.getString(modulePath);
-//                    if (StringUtils.isBlank(preSdkPath) || !StringUtils.equals(preSdkPath, browseButton.getText())) {
-                    PropertiesUtils.setString(modulePath, browseButton.getText());
-                    VirtualFile aarFile = FileUtils.getAarFile(sdkFile);
-                    FileUtils.copyFile(project, aarFile, FileUtils.getModuleLibsFolder(folder), FileUtils.getAarFileName(aarFile));
-                    Log.i("modulePath = " + modulePath);
-                    GradleUtils.addAppBuildFile(project, FileUtils.getAarName(FileUtils.getAarFileName(aarFile)), modulePath);
-                    VirtualFileManager.getInstance().syncRefresh();
-                    ApplicationManager.getApplication().runWriteAction(new Runnable() {
-                        @Override
-                        public void run() {
-                            ModifyAndroidManifest manifest = new ModifyAndroidManifest(project, folder, "");
-                            manifest.modifyManifestXml(ModifyAndroidManifest.ModifyManifestType.NIBIRU_PLUGIN_IDS);
-                        }
-                    });
-//                    }
+                    if (StringUtils.isBlank(preSdkPath) || !StringUtils.equals(preSdkPath, browseButton.getText())) {
+                        PropertiesUtils.setString(modulePath, browseButton.getText());
+                        VirtualFile aarFile = FileUtils.getAarFile(sdkFile);
+                        FileUtils.copyFile(project, aarFile, FileUtils.getModuleLibsFolder(folder), FileUtils.getAarFileName(aarFile));
+                        Log.i("modulePath = " + modulePath);
+                        GradleUtils.addAppBuildFile(project, FileUtils.getAarName(FileUtils.getAarFileName(aarFile)), modulePath);
+                        VirtualFileManager.getInstance().syncRefresh();
+                        ApplicationManager.getApplication().runWriteAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                ModifyAndroidManifest manifest = new ModifyAndroidManifest(project, folder, "");
+                                manifest.modifyManifestXml(ModifyAndroidManifest.ModifyManifestType.NIBIRU_PLUGIN_IDS);
+                            }
+                        });
+                    }
 
                     if (!FileUtils.isInstallExe()) {
                         FileUtils.installExe(FileUtils.getExePath(LocalFileSystem.getInstance().findFileByPath(browseButton.getText())));
